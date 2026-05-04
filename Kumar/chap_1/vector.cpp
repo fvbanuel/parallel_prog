8efc8ee458c28d7482a910371e99ccca5803315f
Francisco Banuelos-Gutierrez <72229317+fvbanuel@users.noreply.github.com>
2026-05-04 16:48:54 -0700

    added optimized grid computation and corrected types to float.

--- Kumar/chap_1/vector.cpp
+++ Kumar/chap_1/vector.cpp
@@ -38,7 +38,7 @@ std::vector<float> prefix_sum(const std::vector<float>& a) {
 }
 
 // add neighbouring blocks on top and right of (i,j)
-void grid_computation(int N, int M, std::vector<std::vector<double>>& grid) {
+void grid_computation(int N, int M, std::vector<std::vector<float>>& grid) {
     #pragma omp parallel for ordered(2)
     for (int i = 1; i < N; i++) {
         for (int j = 1; j < M; j++) {
@@ -52,6 +52,24 @@ void grid_computation(int N, int M, std::vector<std::vector<double>>& grid) {
             #pragma omp ordered depend(source)
         }
     }
+}
+void grid_computation_flat(int N , int M, std:: vector<float>& grid){
+
+    // flat grid index for (i,j) = grid[i*M + j]
+
+    #pragma omp parallel for ordered(2)
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < M; j++) {
+        
+            #pragma omp ordered depend(sink: i-1, j) depend(sink: i, j-1)
+        
+        
+            grid[i * M + j] = grid[(i-1) * M + j] + grid[i * M + (j-1)];
+        
+            #pragma omp ordered depend(source)
+    }
+}
+
 }
 
 
